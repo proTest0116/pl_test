669da169662e2a6460814cb434980510ba273081
watari <WATARI@WATARI.local>
2015-01-16 15:26:20 +0900

    追加

---
+++ setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+		name='Bodies',
+		version='1.0.2'
+		description='Bodies written by Python and PyOpenGL',
+		url='http://www.cc.kyoto-su.ac.jp/~atsushi/',
+		author='AOKI Atsushi',
+		author_email='atsushi@cc.kyoto-su.ac.jp',
+		license='The BSD 2-Clause License',
+		long_description='ドラゴン・スズメバチ・うさぎ・ペンギン・鬼・赤ちゃんを３次元病がする',
+		platforms='OS X (10.9.1')
+		packages=['jp'],
+	)
