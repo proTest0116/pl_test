981bb60eefddddd35e1f3b6c28332d63e314bdf6
watari <WATARI@WATARI.local>
2015-01-16 15:45:32 +0900

    WATARI
    ディレクトリ位置ミスを修正

--- Bodies.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# -*- codinf: utf-8 -*-
-
-import sys
-
-import jp.ac.kyoto_su.cse.pl.Example as example
-
-if __name__ == '__main__';
-		sys.exit(example.main())
---
+++ Bodies/Bodies.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- codinf: utf-8 -*-
+
+import sys
+
+import jp.ac.kyoto_su.cse.pl.Example as example
+
+if __name__ == '__main__';
+		sys.exit(example.main())
